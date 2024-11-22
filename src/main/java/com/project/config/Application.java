2b44cb9997ba0ae5675854b95ca0751b50a44acb
Piyush <piyushsati0409@gmail.com>
2024-11-22 17:32:40 +0530

    added configuration to make it work like a eureka client

--- src/main/java/com/project/config/Application.java
+++ src/main/java/com/project/config/Application.java
@@ -5,13 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@ComponentScan("com.project")
 @SpringBootApplication
 @EntityScan(basePackages = "com.project.model")
 @EnableJpaRepositories(basePackages = "com.project.repository")
+@EnableDiscoveryClient
 public class Application {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
